d8770b993bbf7f2fedba77199e722698f579f0ea
Makhmadiyorov Fakhriddin <makhmadiyorovfakhriddin@gmail.com>
2024-05-20 10:46:47 +0500

    nnne

---
+++ rails-app/db/migrate/20240520054329_create_comments.rb
@@ -0,0 +1,13 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.references :user, null: false, foreign_key: true
+      t.text :message
+      t.integer :label
+      t.string :emoji
+      t.decimal :score
+
+      t.timestamps
+    end
+  end
+end
